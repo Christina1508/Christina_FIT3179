de98692348e50d2258ec48effc5c212864fc32ab
Christina1508 <101304772+Christina1508@users.noreply.github.com>
2023-09-26 21:56:42 +1000

    Update js_map.js

--- js_map.js
+++ js_map.js
@@ -1,5 +1,3 @@
-var vg_1 = "daily_cost_bar_chart.vg.json";
-vg.embed("#bar_chart", vg_1, function(error, result) {
-  // Callback receiving the View instance and parsed Vega spec
-  // result.view is the View, which resides under the '#vis' element
-});
+var vg_1 = "https://raw.githubusercontent.com/Christina1508/Christina_FIT3179/main/vega-lite.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+}).catch(console.error);
