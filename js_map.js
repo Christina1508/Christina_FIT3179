2081d369b198e4df2b816b1decaedb5913d80567
Christina1508 <101304772+Christina1508@users.noreply.github.com>
2023-09-26 22:21:31 +1000

    Update js_map.js

--- js_map.js
+++ js_map.js
@@ -1,3 +1,3 @@
-var vg_1 = "https://raw.githubusercontent.com/Christina1508/Christina_FIT3179/main/vega-lite.json";
+var vg_1 = "https://raw.githubusercontent.com/Christina1508/Christina_FIT3179/main/json_map.topojson";
 vegaEmbed("#vega_map", vg_1).then(function(result) {
 }).catch(console.error);
