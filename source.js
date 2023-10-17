e39df67fa505a94c8e7ac622d0053daac7e28236
Christina1508 <cpra0014@student.monash.edu>
2023-10-17 22:33:52 +1100

    atw

---
+++ source.js
@@ -0,0 +1,10 @@
+
+
+var vega = "map.vg.json";
+vegaEmbed("#choro_map", vega).then(function(result) {
+}).catch(console.error);
+
+
+var vega = "single_bar.vg.json";
+vegaEmbed("#categories", vega).then(function(result) {
+}).catch(console.error);
