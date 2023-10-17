ba2e7b47678140c8c1e2d20d02ad17dcf2368c79
Christina1508 <cpra0014@student.monash.edu>
2023-10-17 23:27:50 +1100

    linked graphs to html

--- source.js
+++ source.js
@@ -8,3 +8,29 @@ vegaEmbed("#choro_map", vega).then(function(result) {
 var vega = "single_bar.vg.json";
 vegaEmbed("#categories", vega).then(function(result) {
 }).catch(console.error);
+
+var vega = "arrival_bar.vg.json";
+vegaEmbed("#arrival", vega).then(function(result) {
+}).catch(console.error);
+
+var vega = "departure_bar.vg.json";
+vegaEmbed("#dept", vega).then(function(result) {
+}).catch(console.error);
+
+var vega = "text_val.vg.json";
+vegaEmbed("#text", vega).then(function(result) {
+}).catch(console.error);
+
+
+var vega = "line_aus.vg.json";
+vegaEmbed("#aus_lines", vega).then(function(result) {
+}).catch(console.error);
+
+
+var vega = "expend_line.vg.json";
+vegaEmbed("#expend_line", vega).then(function(result) {
+}).catch(console.error);
+
+var vega = "expend_bar.vg.json";
+vegaEmbed("#expend_bar", vega).then(function(result) {
+}).catch(console.error);
