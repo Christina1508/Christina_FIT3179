7a218ee9a765d51e602cb863d5de0484805748e9
Christina1508 <cpra0014@student.monash.edu>
2023-10-18 23:21:30 +1100

    finsihed

--- source.js
+++ source.js
@@ -1,32 +1,34 @@
-
-
-var vega = "map.vg.json";
-vegaEmbed("#choro_map", vega).then(function(result) {
+var vega1 = "map.vg.json";
+vegaEmbed("#choro_map", vega1).then(function(result) {
 }).catch(console.error);
 
 
-var vega = "single_bar.vg.json";
-vegaEmbed("#categories", vega).then(function(result) {
+var vega2 = "single_bar.vg.json";
+vegaEmbed("#categories", vega2).then(function(result) {
 }).catch(console.error);
 
-var vega = "arrival_bar.vg.json";
-vegaEmbed("#arrival", vega).then(function(result) {
+var vega3 = "arrival_bar.vg.json";
+vegaEmbed("#arrival", vega3).then(function(result) {
 }).catch(console.error);
 
-var vega = "text_val.vg.json";
-vegaEmbed("#text", vega).then(function(result) {
-}).catch(console.error);
-
-
-var vega = "line_aus.vg.json";
-vegaEmbed("#aus_lines", vega).then(function(result) {
+var vega4 = "text_val.vg.json";
+vegaEmbed("#text", vega4).then(function(result) {
 }).catch(console.error);
 
 
-var vega = "expend_line.vg.json";
-vegaEmbed("#expend_line", vega).then(function(result) {
+var vega5 = "line_aus.vg.json";
+vegaEmbed("#aus_lines", vega5).then(function(result) {
 }).catch(console.error);
 
-var vega = "expend_bar.vg.json";
-vegaEmbed("#expend_bar", vega).then(function(result) {
+
+var vega6 = "expend_line.vg.json";
+vegaEmbed("#expend_line", vega6).then(function(result) {
+}).catch(console.error);
+
+var vega7 = "expend_bar.vg.json";
+vegaEmbed("#expend_bar", vega7).then(function(result) {
+}).catch(console.error);
+
+var vega = "dot_states.vg.json";
+vegaEmbed("#dot_bar", vega).then(function(result) {
 }).catch(console.error);
